29622814f97340e20745f705a31ef47f33222b3c
Farhad <120501202+JoyJinx@users.noreply.github.com>
2023-08-26 21:15:49 +0330

    update js

--- javascript.js
+++ javascript.js
@@ -3,9 +3,6 @@ $(document).ready(function () {
     window.alert("This would have work if there was a server!");
   });
   $("#subscribe button[type=button]").click(function () {
-    const answer = window.prompt("Are you sure you want to become a batbro?");
-    answer.toLowerCase() == "yes"
-      ? window.alert("you are a good man!")
-      : window.alert("you can be better...");
+    window.alert("This would have worked too...");
   });
 });
