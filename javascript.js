1cdd9cd61fc2fe3d3dd9d44828aab32b1f398e9e
Farhad <120501202+JoyJinx@users.noreply.github.com>
2023-09-01 21:39:52 +0330

    update js

--- javascript.js
+++ javascript.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
     window.alert("This would have work if there was a server!");
   });
   $("#subscribe button[type=button]").click(function () {
-    window.alert("This would have worked too...");
+    window.alert("This would have worked too ...");
   });
 });
