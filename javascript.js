e43485c57e993518cdb02323a6620b2ae9158bad
Farhad <120501202+JoyJinx@users.noreply.github.com>
2023-09-13 21:20:41 +0330

    update js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".download-btn").click(function () {
-    window.alert("This would have work if there was a server!");
+    window.alert("This would have work if there was a server!!!");
   });
   $("#subscribe button[type=button]").click(function () {
     window.alert("This would have worked too...");
